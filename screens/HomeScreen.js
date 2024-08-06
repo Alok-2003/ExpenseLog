16bce24feb974c9de7512e9fb3689e33c1ed1f41
Alok-2003 <alok953280@gmail.com>
2024-08-07 01:10:04 +0530

    Basic Setup done

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View className="mt-10" >
+      <Text className="text-5xl bg-yellow-500" >Alok kfucker boy</Text>
+    </View>
+  )
+}
+
+//rnf
---
+++ screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../components/ScreenWrapper'
+
+export default function LoginScreen() {
+  return (
+    <ScreenWrapper className="" >
+      <Text>LoginScreen</Text>
+    </ScreenWrapper>
+  )
+}
