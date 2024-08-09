1949037c1caf71dccc6aed9069e424ee2136f08f
Alok-2003 <alok953280@gmail.com>
2024-08-09 19:23:45 +0530

    HomeScreen done

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,40 @@
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import ScreenWrapper from '../components/ScreenWrapper';
+import {colors} from '../theme';
+import { useNavigation } from '@react-navigation/native';
+
+export default function WelcomeScreen() {
+    const navigation=useNavigation();
+  return (
+    <ScreenWrapper>
+      <View className="h-full flex justify-around">
+        <View className="flex-row justify-center mt-10">
+          <Image
+            source={require('../assets/images/welcome.gif')}
+            className="h-96 w-96 shadow-xl"
+          />
+        </View>
+        <View className="mx-5 mb-20">
+          <Text
+            className={`${colors.heading} text-center font-bold text-4xl mb-10`}>
+            ExpenseLog
+          </Text>
+          <TouchableOpacity onPress={()=> navigation.navigate('SignIn')} className="shadow p-3 rounded-full mb-7" style={{backgroundColor:colors.button}}  >
+            <Text
+              className={`${colors.heading} text-center text-white font-bold text-lg `}>
+              Sign In
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=> navigation.navigate('SignUp')} className="shadow p-3 rounded-full " style={{backgroundColor:colors.button}}  >
+            <Text
+              className={`${colors.heading} text-center text-white font-bold text-lg `}>
+              Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+}
